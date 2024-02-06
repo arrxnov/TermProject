cb503b861c4bab4b7d2239f0043f0941f36dcacd
Kai <manicdragon12@gmail.com>
2024-02-06 13:18:03 -0500

    More form progress, added RE checking

--- project1/form.js
+++ project1/form.js
@@ -7,7 +7,7 @@ Student name
 Current semester (all prior semesters considered “history.”)
 An array of Course objects, each with term, year, course designator, and course name.
 
-Convert the Course objects into a new Year object, which contains Terms containing Courses. In other words, just re-organize the data by year and term.
+
 
 Replace the static HTML in the UR div with dynamic data stored in your Year objects. Appearance should remain the same, and still use the same CSS.
 */
@@ -17,7 +17,8 @@ Course = {
     year: "",
     course_des: "",
     course_name: ""
-}
+};
+
 Plan = {
     plan_name: "",
     catalog_year: "",
@@ -25,6 +26,33 @@ Plan = {
     student_name: "",
     current_semester: "",
     courses: {}
-}
+};
 
-Year
+
+
+/*
+Convert the Course objects into a new Year object, which contains Terms containing Courses. In other words, just re-organize the data by year and term.
+*/
+Year = {
+    year: "",
+    spring: {},
+    summer: {},
+    fall: {}
+};
+
+function validateForm() {
+    const termRE = new RegExp("((S|s)pring|(S|s)ummer|(F|f)all)");
+    const yearRE = new RegExp("[1-2][0-9]{3}");
+
+    let form = document.forms["courseform"]
+    if (form["coursenum"].value == "") {
+        alert("Course Number must be filled out");
+        return false;
+    } else if (form["coursename"].value == "") {
+        alert("Course Name must be filled out");
+        return false;
+    } else if (termRE.test(form["term"])) {
+
+    } else if (yearRE.test(form["year"])) {
+    }
+}
