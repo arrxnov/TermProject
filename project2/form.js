19259e46b3ca44ef19f5125b53c9f192610b5120
Kai <manicdragon12@gmail.com>
2024-02-06 15:23:14 -0500

    Working on JS form and validation

--- project2/form.js
+++ project2/form.js
@@ -11,7 +11,7 @@ An array of Course objects, each with term, year, course designator, and course 
 
 Replace the static HTML in the UR div with dynamic data stored in your Year objects. Appearance should remain the same, and still use the same CSS.
 */
-
+/*
 Course = {
     term: "",
     year: "",
@@ -28,6 +28,7 @@ Plan = {
     current_semester: "",
     courses: {}
 };
+*/
 
 let data = {};
 
@@ -66,6 +67,7 @@ function planToYear(planJSON) {
 
 function updateCourses(years) {
     let header = document.getElementById("#ur-header");
+    // TODO: set header values (other than total hours)
     /*
     <div id="ur-header" class="labels">
         <p><strong>Student:</strong> loganmiller216</p>
@@ -74,7 +76,8 @@ function updateCourses(years) {
     </div>
     */
 
-   let header2 = document.getElementById("#ur-header-2");
+    let header2 = document.getElementById("#ur-header-2");
+    // TODO: set header values
     /*
     <div id="ur-header-2" class="labels">
         <p><strong>Major:</strong> Computer Science, Cyber Operations</p>
@@ -84,47 +87,97 @@ function updateCourses(years) {
         <p><strong>Major GPA:</strong> 3.55</p>
     </div>
     */
+    let totalCreds = 0;
 
+    for (let i=1; i<13; i++) {
+        let semester = document.getElementById("#semester"+i);
+        let base_y = years["9999"]["catalog_year"];
+        let y = base_y + int((i+1)/3);
+        let t;
+        let term;
+        switch(i%3) {
+            case 0: 
+                t = "SU";
+                term = "Summer";
+                break;
+            case 1:
+                t = "FA";
+                term = "Fall";
+                break;
+            case 2:
+                t = "SP";
+                t = "Spring";
+                break; 
+        }
+        term += " " + y;
+
+        let courses = years[y][t];
+        let credits = 0;
+        for (let course in courses) {
+            credits += course["credits"];
+            // TODO: append course to semester div
+        }
+        totalCreds += credits;
+        // TODO: set credits div
+
+        /*
+        <div id="semester1" class="semester fall">
+            <div class="semester-hdr">
+                <div class="term">Fall 2021</div>
+                <div class="credits">Credits: 16</div>
+            </div>
+            <p>MATH-1710 Calculus I</p>
+            <p>CS-1210 C++ Programming</p>
+            <p>EGCP-1010 Digital Logic Design</p>
+            <p>COM-1100 Fundamentals of Speech</p>
+            <p>BTGE-1725 Bible & the Gospel</p>
+        </div>
+        */
+
+        // TODO: set total credits value in header
+
+    }
     
 }
 
 
-/*
-fetch('C:\Users\\manic\\Documents\\GitHub\\web-term-project\\project2\\plan.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log('Data from JSON file:', data);
-    })
-    .catch(error => {
-        console.error('Error reading file:', error);
-    })
 
-    
-console.log('Real data from JSON file:', data);
-*/
-
-/*
-Convert the Course objects into a new Year object, which contains Terms containing Courses. In other words, just re-organize the data by year and term.
-*/
-
-
+// TODO: figure out why this doesn't validate properly
 function validateForm() {
+    console.log("Validating");
     const termRE = new RegExp("(SP|SU|FA)");
     const yearRE = new RegExp("[0-2][0-9]{3}");
 
-    let form = document.forms["courseform"]
-    if (form["coursenum"].value == "") {
+    const deptRE = new RegExp("[A-Z]{2,5}-[0-9]{4}");
+
+    // TODO: combine the dept des and number form fields
+
+    let form = document.forms["course_finder"]
+    if (!termRE.test(form["course_dept"])) {
+        alert("Course Dept. invalid");
+        console.log("course_dept invalid");
+        return false;
+    } 
+    
+    else if (form["course_title"].value == "") {
+        alert("Course Title must be filled out");
+        console.log("course_title empty");
+        return false;
+    }
+
+
+    else if (form["course_num"].value == "") {
         alert("Course Number must be filled out");
+        console.log("coursenum empty");
         return false;
-    } else if (form["coursename"].value == "") {
-        alert("Course Name must be filled out");
-        return false;
-    } else if (!termRE.test(form["term"])) {
+    }  else if (!termRE.test(form["term"])) {
         alert("Invalid course term");
-        return 
+        console.log("term empty");
+        return false;
     } else if (!yearRE.test(form["year"])) {
         alert("Invalid course year");
-        return 
+        console.log("year empty");
+        return false;
     }
 }
 
