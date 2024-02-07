d2558dfe433e420b0982e41ca6981f346d32bb1b
arrxnov <jcbgrd@gmail.com>
2024-02-07 09:51:01 -0500

    Add header data

--- project2/js/form.js
+++ project2/js/form.js
@@ -11,7 +11,7 @@ An array of Course objects, each with term, year, course designator, and course 
 
 Replace the static HTML in the UR div with dynamic data stored in your Year objects. Appearance should remain the same, and still use the same CSS.
 */
-/*
+
 Course = {
     term: "",
     year: "",
@@ -24,11 +24,12 @@ Plan = {
     plan_name: "",
     catalog_year: "",
     major: "",
+    minor: "",
+    gpa: "",
     student_name: "",
     current_semester: "",
     courses: {}
 };
-*/
 
 let data = {};
 
@@ -39,6 +40,9 @@ function planToYear(planJSON) {
             plan_name:          planJSON["plan_name"],
             catalog_year:       planJSON["catalog_year"],
             major:              planJSON["major"],
+            minor:              planJSON["minor"],
+            gpa:                planJSON["gpa"],
+            major_gpa:          planJSON["major_gpa"],
             student_name:       planJSON["student_name"],
             current_semester:   planJSON["current_semester"],
         }
@@ -68,6 +72,9 @@ function planToYear(planJSON) {
 function updateCourses(years) {
     let header = document.getElementById("#ur-header");
     // TODO: set header values (other than total hours)
+    
+    header.innerHTML += "<p><strong>Student</strong> " + years["9999"].student_name + "</p>\n";
+    header.innerHTML += "<p><strong>Course Plan:</strong> " + years["9999"].year + "</p>\n";
     /*
     <div id="ur-header" class="labels">
         <p><strong>Student:</strong> loganmiller216</p>
@@ -78,6 +85,12 @@ function updateCourses(years) {
 
     let header2 = document.getElementById("#ur-header-2");
     // TODO: set header values
+    header2.innerHTML += "<p><strong>Major:</strong> " + years["9999"].major + "</p>\n";
+    header2.innerHTML += "<p><strong>Minor:</strong> " + years["9999"].minor + "</p>\n";
+    header2.innerHTML += "<p><strong>Catalog:</strong> " + years["9999"].catalog_year + "</p>\n";
+    header2.innerHTML += "<p><strong>GPA:</strong> " + years["9999"].gpa + "</p>\n";
+    header2.innerHTML += "<p><strong>Major GPA:</strong> " + years["9999"].major_gpa + "</p>\n";
+    
     /*
     <div id="ur-header-2" class="labels">
         <p><strong>Major:</strong> Computer Science, Cyber Operations</p>
@@ -106,7 +119,7 @@ function updateCourses(years) {
                 break;
             case 2:
                 t = "SP";
-                t = "Spring";
+                term = "Spring";
                 break; 
         }
         term += " " + y;
@@ -115,10 +128,13 @@ function updateCourses(years) {
         let credits = 0;
         for (let course in courses) {
             credits += course["credits"];
-            // TODO: append course to semester div
+            // DONE: append course to semester div
+            semester.innerHTML += "<p>" + course.course_des + " " + course.course_name + "</p>\n";
         }
         totalCreds += credits;
-        // TODO: set credits div
+        // DONE: set credits div
+        let year = semester.getElementsByClassName("credits")[0];
+        year.textContent = "Credits: " + credits;
 
         /*
         <div id="semester1" class="semester fall">
@@ -133,11 +149,9 @@ function updateCourses(years) {
             <p>BTGE-1725 Bible & the Gospel</p>
         </div>
         */
-
-        // TODO: set total credits value in header
-
+        
     }
-    
+    header.innerHTML += "<p><strong>Total Hours:</strong> " + totalCreds + "</p>\n";
 }
 
 
