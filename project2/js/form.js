07dc98d42a6e811b75d510e6bae00e7e53fa62e1
Logan Miller <lj@mountainmillers.com>
2024-02-07 08:46:47 -0500

    Worked on form validation

--- project2/js/form.js
+++ project2/js/form.js
@@ -181,3 +181,23 @@ function validateForm() {
     }
 }
 
+
+// let zipCodeValid = false;
+// let zipCodeWidget = document.querySelector("#zip");
+// zipCodeWidget.addEventListener("input", checkZipCode);
+
+// function checkZipCode() {
+//    let regex = /^\d\d\d\d\d$/;
+//    let zip = zipCodeWidget.value.trim();
+//    zipCodeValid = zip.match(regex);
+// }
+
+// let tosForm = document.querySelector("#tosForm");
+// tosForm.addEventListener("submit", checkForm);
+
+// function checkForm(event) {
+//    if (!zipCodeValid) {
+//       event.preventDefault();
+//    }
+// }
+
