9da2b5c015f9d94ef8790fe7281c1f77b7861958
Logan Miller <lj@mountainmillers.com>
2024-02-22 00:00:46 -0500

    working on search table

--- project3/js/form.js
+++ project3/js/form.js
@@ -336,5 +336,8 @@ jQuery(document).ready(function() {
     }
 
     // add code to create new dataTable
-    
+    let table = jQuery('#searchTable').dataTable().api(); //dataTable() returns a jQuery object
+
+    table.rows.add(getCombined())
+
 });
