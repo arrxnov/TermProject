523f2b7e3d86d10acbfdc2202d692695bedf964a
Logan Miller <lj@mountainmillers.com>
2024-03-02 17:30:27 -0800

    updated database files

--- project4/database/create-zeus.sql
+++ project4/database/create-zeus.sql
@@ -1,238 +1,238 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2024-03-03 00:52:12.5
+-- Last modification date: 2024-03-03 01:26:57.523
 
 -- tables
--- Table: catalog
-CREATE TABLE catalog (
+-- Table: zeus_catalog
+CREATE TABLE zeus_catalog (
     year numeric(4,0)  NOT NULL,
-    CONSTRAINT catalog_pk PRIMARY KEY (year)
+    CONSTRAINT zeus_catalog_pk PRIMARY KEY (year)
 );
 
--- Table: catalog_course
-CREATE TABLE catalog_course (
+-- Table: zeus_catalog_course
+CREATE TABLE zeus_catalog_course (
     catalog_year numeric(4,0)  NOT NULL,
     course_id varchar(9)  NOT NULL,
-    CONSTRAINT catalog_course_pk PRIMARY KEY (course_id,catalog_year)
+    CONSTRAINT zeus_catalog_course_pk PRIMARY KEY (course_id,catalog_year)
 );
 
--- Table: concentration
-CREATE TABLE concentration (
+-- Table: zeus_concentration
+CREATE TABLE zeus_concentration (
     id int  NOT NULL AUTO_INCREMENT,
     name varchar(32)  NOT NULL,
     major_id int  NOT NULL,
-    CONSTRAINT concentration_pk PRIMARY KEY (id)
+    CONSTRAINT zeus_concentration_pk PRIMARY KEY (id)
 );
 
--- Table: concentration_course
-CREATE TABLE concentration_course (
+-- Table: zeus_concentration_course
+CREATE TABLE zeus_concentration_course (
     concentration_id int  NOT NULL,
     course_id varchar(9)  NOT NULL,
     type varchar(9)  NOT NULL CHECK (type in ('core', 'elective')),
-    CONSTRAINT concentration_course_pk PRIMARY KEY (course_id,concentration_id)
+    CONSTRAINT zeus_concentration_course_pk PRIMARY KEY (course_id,concentration_id)
 );
 
--- Table: course
-CREATE TABLE course (
+-- Table: zeus_course
+CREATE TABLE zeus_course (
     id varchar(9)  NOT NULL,
     name varchar(32)  NOT NULL,
     credits numeric(3,1)  NOT NULL,
     description varchar(512)  NOT NULL,
-    CONSTRAINT course_pk PRIMARY KEY (id)
+    CONSTRAINT zeus_course_pk PRIMARY KEY (id)
 );
 
--- Table: gened
-CREATE TABLE gened (
+-- Table: zeus_gened
+CREATE TABLE zeus_gened (
     catalog_year numeric(4,0)  NOT NULL,
     course_id varchar(9)  NOT NULL,
     type varchar(9)  NOT NULL CHECK (type in ('core', 'elective')),
-    CONSTRAINT gened_pk PRIMARY KEY (course_id,catalog_year)
+    CONSTRAINT zeus_gened_pk PRIMARY KEY (course_id,catalog_year)
 );
 
--- Table: major
-CREATE TABLE major (
+-- Table: zeus_major
+CREATE TABLE zeus_major (
     id int  NOT NULL AUTO_INCREMENT,
     name varchar(32)  NOT NULL,
     catalog_year numeric(4,0)  NOT NULL,
-    CONSTRAINT major_pk PRIMARY KEY (id)
+    CONSTRAINT zeus_major_pk PRIMARY KEY (id)
 );
 
--- Table: major_course
-CREATE TABLE major_course (
+-- Table: zeus_major_course
+CREATE TABLE zeus_major_course (
     major_id int  NOT NULL,
     course_id varchar(9)  NOT NULL,
     type varchar(9)  NOT NULL CHECK (type in ('core', 'elective')),
-    CONSTRAINT major_course_pk PRIMARY KEY (course_id,major_id)
+    CONSTRAINT zeus_major_course_pk PRIMARY KEY (course_id,major_id)
 );
 
--- Table: minor
-CREATE TABLE minor (
+-- Table: zeus_minor
+CREATE TABLE zeus_minor (
     id int  NOT NULL AUTO_INCREMENT,
     name varchar(32)  NOT NULL,
     catalog_year numeric(4,0)  NOT NULL,
-    CONSTRAINT minor_pk PRIMARY KEY (id)
+    CONSTRAINT zeus_minor_pk PRIMARY KEY (id)
 );
 
--- Table: minor_course
-CREATE TABLE minor_course (
+-- Table: zeus_minor_course
+CREATE TABLE zeus_minor_course (
     minor_id int  NOT NULL,
     course_id varchar(9)  NOT NULL,
     type varchar(9)  NOT NULL CHECK (type in ('core', 'elective')),
-    CONSTRAINT minor_course_pk PRIMARY KEY (course_id,minor_id)
+    CONSTRAINT zeus_minor_course_pk PRIMARY KEY (course_id,minor_id)
 );
 
--- Table: plan
-CREATE TABLE plan (
+-- Table: zeus_plan
+CREATE TABLE zeus_plan (
     id int  NOT NULL AUTO_INCREMENT,
     name varchar(32)  NOT NULL,
     catalog_year numeric(4,0)  NOT NULL,
     user_id int  NOT NULL,
-    CONSTRAINT plan_pk PRIMARY KEY (id)
+    CONSTRAINT zeus_plan_pk PRIMARY KEY (id)
 );
 
--- Table: planned_concentration
-CREATE TABLE planned_concentration (
+-- Table: zeus_planned_concentration
+CREATE TABLE zeus_planned_concentration (
     concentration_id int  NOT NULL,
     plan_id int  NOT NULL,
-    CONSTRAINT planned_concentration_pk PRIMARY KEY (concentration_id,plan_id)
+    CONSTRAINT zeus_planned_concentration_pk PRIMARY KEY (concentration_id,plan_id)
 );
 
--- Table: planned_course
-CREATE TABLE planned_course (
+-- Table: zeus_planned_course
+CREATE TABLE zeus_planned_course (
     plan_id int  NOT NULL,
     course_id varchar(9)  NOT NULL,
     year numeric(4,0)  NOT NULL,
     term varchar(6)  NOT NULL CHECK (semester in ('Fall', 'Spring', 'Summer')),
-    CONSTRAINT planned_course_pk PRIMARY KEY (plan_id,course_id,year,term)
+    CONSTRAINT zeus_planned_course_pk PRIMARY KEY (plan_id,course_id,year,term)
 );
 
--- Table: planned_major
-CREATE TABLE planned_major (
+-- Table: zeus_planned_major
+CREATE TABLE zeus_planned_major (
     major_id int  NOT NULL,
     plan_id int  NOT NULL,
-    CONSTRAINT planned_major_pk PRIMARY KEY (major_id,plan_id)
+    CONSTRAINT zeus_planned_major_pk PRIMARY KEY (major_id,plan_id)
 );
 
--- Table: planned_minor
-CREATE TABLE planned_minor (
+-- Table: zeus_planned_minor
+CREATE TABLE zeus_planned_minor (
     minor_id int  NOT NULL,
     plan_id int  NOT NULL,
-    CONSTRAINT planned_minor_pk PRIMARY KEY (minor_id,plan_id)
+    CONSTRAINT zeus_planned_minor_pk PRIMARY KEY (minor_id,plan_id)
 );
 
--- Table: prereq
-CREATE TABLE prereq (
+-- Table: zeus_prereq
+CREATE TABLE zeus_prereq (
     course_id varchar(9)  NOT NULL,
     prereq_id varchar(9)  NOT NULL,
-    CONSTRAINT prereq_pk PRIMARY KEY (course_id,prereq_id)
+    CONSTRAINT zeus_prereq_pk PRIMARY KEY (course_id,prereq_id)
 );
 
--- Table: user
-CREATE TABLE user (
+-- Table: zeus_user
+CREATE TABLE zeus_user (
     id int  NOT NULL AUTO_INCREMENT,
     username varchar(32)  NOT NULL,
     phash char(60)  NOT NULL,
     name varchar(64)  NOT NULL,
     gpa numeric(3,2)  NOT NULL,
     major_gpa numeric(3,2)  NOT NULL,
-    CONSTRAINT user_pk PRIMARY KEY (id)
+    CONSTRAINT zeus_user_pk PRIMARY KEY (id)
 );
 
 -- foreign keys
--- Reference: catalog_catalog (table: catalog_course)
-ALTER TABLE catalog_course ADD CONSTRAINT catalog_catalog FOREIGN KEY catalog_catalog (catalog_year)
-    REFERENCES catalog (year);
+-- Reference: catalog_catalog (table: zeus_catalog_course)
+ALTER TABLE zeus_catalog_course ADD CONSTRAINT catalog_catalog FOREIGN KEY catalog_catalog (catalog_year)
+    REFERENCES zeus_catalog (year);
 
--- Reference: catalog_course (table: catalog_course)
-ALTER TABLE catalog_course ADD CONSTRAINT catalog_course FOREIGN KEY catalog_course (course_id)
-    REFERENCES course (id);
+-- Reference: catalog_course (table: zeus_catalog_course)
+ALTER TABLE zeus_catalog_course ADD CONSTRAINT catalog_course FOREIGN KEY catalog_course (course_id)
+    REFERENCES zeus_course (id);
 
--- Reference: concentration_course_concentration (table: concentration_course)
-ALTER TABLE concentration_course ADD CONSTRAINT concentration_course_concentration FOREIGN KEY concentration_course_concentration (concentration_id)
-    REFERENCES concentration (id);
+-- Reference: concentration_course_concentration (table: zeus_concentration_course)
+ALTER TABLE zeus_concentration_course ADD CONSTRAINT concentration_course_concentration FOREIGN KEY concentration_course_concentration (concentration_id)
+    REFERENCES zeus_concentration (id);
 
--- Reference: concentration_courses_course (table: concentration_course)
-ALTER TABLE concentration_course ADD CONSTRAINT concentration_courses_course FOREIGN KEY concentration_courses_course (course_id)
-    REFERENCES course (id);
+-- Reference: concentration_courses_course (table: zeus_concentration_course)
+ALTER TABLE zeus_concentration_course ADD CONSTRAINT concentration_courses_course FOREIGN KEY concentration_courses_course (course_id)
+    REFERENCES zeus_course (id);
 
--- Reference: concentrations_majors (table: concentration)
-ALTER TABLE concentration ADD CONSTRAINT concentrations_majors FOREIGN KEY concentrations_majors (major_id)
-    REFERENCES major (id);
+-- Reference: concentrations_majors (table: zeus_concentration)
+ALTER TABLE zeus_concentration ADD CONSTRAINT concentrations_majors FOREIGN KEY concentrations_majors (major_id)
+    REFERENCES zeus_major (id);
 
--- Reference: gened_catalog (table: gened)
-ALTER TABLE gened ADD CONSTRAINT gened_catalog FOREIGN KEY gened_catalog (catalog_year)
-    REFERENCES catalog (year);
+-- Reference: gened_catalog (table: zeus_gened)
+ALTER TABLE zeus_gened ADD CONSTRAINT gened_catalog FOREIGN KEY gened_catalog (catalog_year)
+    REFERENCES zeus_catalog (year);
 
--- Reference: gened_course (table: gened)
-ALTER TABLE gened ADD CONSTRAINT gened_course FOREIGN KEY gened_course (course_id)
-    REFERENCES course (id);
+-- Reference: gened_course (table: zeus_gened)
+ALTER TABLE zeus_gened ADD CONSTRAINT gened_course FOREIGN KEY gened_course (course_id)
+    REFERENCES zeus_course (id);
 
--- Reference: major_catalog (table: major)
-ALTER TABLE major ADD CONSTRAINT major_catalog FOREIGN KEY major_catalog (catalog_year)
-    REFERENCES catalog (year);
+-- Reference: major_catalog (table: zeus_major)
+ALTER TABLE zeus_major ADD CONSTRAINT major_catalog FOREIGN KEY major_catalog (catalog_year)
+    REFERENCES zeus_catalog (year);
 
--- Reference: major_course (table: major_course)
-ALTER TABLE major_course ADD CONSTRAINT major_course FOREIGN KEY major_course (course_id)
-    REFERENCES course (id);
+-- Reference: major_course (table: zeus_major_course)
+ALTER TABLE zeus_major_course ADD CONSTRAINT major_course FOREIGN KEY major_course (course_id)
+    REFERENCES zeus_course (id);
 
--- Reference: major_courses (table: major_course)
-ALTER TABLE major_course ADD CONSTRAINT major_courses FOREIGN KEY major_courses (major_id)
-    REFERENCES major (id);
+-- Reference: major_courses (table: zeus_major_course)
+ALTER TABLE zeus_major_course ADD CONSTRAINT major_courses FOREIGN KEY major_courses (major_id)
+    REFERENCES zeus_major (id);
 
--- Reference: minor_catalog (table: minor)
-ALTER TABLE minor ADD CONSTRAINT minor_catalog FOREIGN KEY minor_catalog (catalog_year)
-    REFERENCES catalog (year);
+-- Reference: minor_catalog (table: zeus_minor)
+ALTER TABLE zeus_minor ADD CONSTRAINT minor_catalog FOREIGN KEY minor_catalog (catalog_year)
+    REFERENCES zeus_catalog (year);
 
--- Reference: minor_courses (table: minor_course)
-ALTER TABLE minor_course ADD CONSTRAINT minor_courses FOREIGN KEY minor_courses (minor_id)
-    REFERENCES minor (id);
+-- Reference: minor_courses (table: zeus_minor_course)
+ALTER TABLE zeus_minor_course ADD CONSTRAINT minor_courses FOREIGN KEY minor_courses (minor_id)
+    REFERENCES zeus_minor (id);
 
--- Reference: minor_courses_course (table: minor_course)
-ALTER TABLE minor_course ADD CONSTRAINT minor_courses_course FOREIGN KEY minor_courses_course (course_id)
-    REFERENCES course (id);
+-- Reference: minor_courses_course (table: zeus_minor_course)
+ALTER TABLE zeus_minor_course ADD CONSTRAINT minor_courses_course FOREIGN KEY minor_courses_course (course_id)
+    REFERENCES zeus_course (id);
 
--- Reference: plan_user (table: plan)
-ALTER TABLE plan ADD CONSTRAINT plan_user FOREIGN KEY plan_user (user_id)
-    REFERENCES user (id);
+-- Reference: plan_user (table: zeus_plan)
+ALTER TABLE zeus_plan ADD CONSTRAINT plan_user FOREIGN KEY plan_user (user_id)
+    REFERENCES zeus_user (id);
 
--- Reference: planned_concentration_concentration (table: planned_concentration)
-ALTER TABLE planned_concentration ADD CONSTRAINT planned_concentration_concentration FOREIGN KEY planned_concentration_concentration (concentration_id)
-    REFERENCES concentration (id);
+-- Reference: planned_concentration_concentration (table: zeus_planned_concentration)
+ALTER TABLE zeus_planned_concentration ADD CONSTRAINT planned_concentration_concentration FOREIGN KEY planned_concentration_concentration (concentration_id)
+    REFERENCES zeus_concentration (id);
 
--- Reference: planned_concentration_plan (table: planned_concentration)
-ALTER TABLE planned_concentration ADD CONSTRAINT planned_concentration_plan FOREIGN KEY planned_concentration_plan (plan_id)
-    REFERENCES plan (id);
+-- Reference: planned_concentration_plan (table: zeus_planned_concentration)
+ALTER TABLE zeus_planned_concentration ADD CONSTRAINT planned_concentration_plan FOREIGN KEY planned_concentration_plan (plan_id)
+    REFERENCES zeus_plan (id);
 
--- Reference: planned_courses_course (table: planned_course)
-ALTER TABLE planned_course ADD CONSTRAINT planned_courses_course FOREIGN KEY planned_courses_course (course_id)
-    REFERENCES course (id);
+-- Reference: planned_courses_course (table: zeus_planned_course)
+ALTER TABLE zeus_planned_course ADD CONSTRAINT planned_courses_course FOREIGN KEY planned_courses_course (course_id)
+    REFERENCES zeus_course (id);
 
--- Reference: planned_courses_plan (table: planned_course)
-ALTER TABLE planned_course ADD CONSTRAINT planned_courses_plan FOREIGN KEY planned_courses_plan (plan_id)
-    REFERENCES plan (id);
+-- Reference: planned_courses_plan (table: zeus_planned_course)
+ALTER TABLE zeus_planned_course ADD CONSTRAINT planned_courses_plan FOREIGN KEY planned_courses_plan (plan_id)
+    REFERENCES zeus_plan (id);
 
--- Reference: planned_major_major (table: planned_major)
-ALTER TABLE planned_major ADD CONSTRAINT planned_major_major FOREIGN KEY planned_major_major (major_id)
-    REFERENCES major (id);
+-- Reference: planned_major_major (table: zeus_planned_major)
+ALTER TABLE zeus_planned_major ADD CONSTRAINT planned_major_major FOREIGN KEY planned_major_major (major_id)
+    REFERENCES zeus_major (id);
 
--- Reference: planned_major_plan (table: planned_major)
-ALTER TABLE planned_major ADD CONSTRAINT planned_major_plan FOREIGN KEY planned_major_plan (plan_id)
-    REFERENCES plan (id);
+-- Reference: planned_major_plan (table: zeus_planned_major)
+ALTER TABLE zeus_planned_major ADD CONSTRAINT planned_major_plan FOREIGN KEY planned_major_plan (plan_id)
+    REFERENCES zeus_plan (id);
 
--- Reference: planned_minor_minor (table: planned_minor)
-ALTER TABLE planned_minor ADD CONSTRAINT planned_minor_minor FOREIGN KEY planned_minor_minor (minor_id)
-    REFERENCES minor (id);
+-- Reference: planned_minor_minor (table: zeus_planned_minor)
+ALTER TABLE zeus_planned_minor ADD CONSTRAINT planned_minor_minor FOREIGN KEY planned_minor_minor (minor_id)
+    REFERENCES zeus_minor (id);
 
--- Reference: planned_minor_plan (table: planned_minor)
-ALTER TABLE planned_minor ADD CONSTRAINT planned_minor_plan FOREIGN KEY planned_minor_plan (plan_id)
-    REFERENCES plan (id);
+-- Reference: planned_minor_plan (table: zeus_planned_minor)
+ALTER TABLE zeus_planned_minor ADD CONSTRAINT planned_minor_plan FOREIGN KEY planned_minor_plan (plan_id)
+    REFERENCES zeus_plan (id);
 
--- Reference: prereqs_course1 (table: prereq)
-ALTER TABLE prereq ADD CONSTRAINT prereqs_course1 FOREIGN KEY prereqs_course1 (course_id)
-    REFERENCES course (id);
+-- Reference: prereqs_course1 (table: zeus_prereq)
+ALTER TABLE zeus_prereq ADD CONSTRAINT prereqs_course1 FOREIGN KEY prereqs_course1 (course_id)
+    REFERENCES zeus_course (id);
 
--- Reference: prereqs_course2 (table: prereq)
-ALTER TABLE prereq ADD CONSTRAINT prereqs_course2 FOREIGN KEY prereqs_course2 (prereq_id)
-    REFERENCES course (id);
+-- Reference: prereqs_course2 (table: zeus_prereq)
+ALTER TABLE zeus_prereq ADD CONSTRAINT prereqs_course2 FOREIGN KEY prereqs_course2 (prereq_id)
+    REFERENCES zeus_course (id);
 
 -- End of file.
 
--- project4/database/drop-zeus.sql
+++ project4/database/drop-zeus.sql
@@ -1,113 +1,113 @@
 -- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2024-03-03 00:52:12.5
+-- Last modification date: 2024-03-03 01:26:57.523
 
 -- foreign keys
-ALTER TABLE catalog_course
+ALTER TABLE zeus_catalog_course
     DROP FOREIGN KEY catalog_catalog;
 
-ALTER TABLE catalog_course
+ALTER TABLE zeus_catalog_course
     DROP FOREIGN KEY catalog_course;
 
-ALTER TABLE concentration_course
+ALTER TABLE zeus_concentration_course
     DROP FOREIGN KEY concentration_course_concentration;
 
-ALTER TABLE concentration_course
+ALTER TABLE zeus_concentration_course
     DROP FOREIGN KEY concentration_courses_course;
 
-ALTER TABLE concentration
+ALTER TABLE zeus_concentration
     DROP FOREIGN KEY concentrations_majors;
 
-ALTER TABLE gened
+ALTER TABLE zeus_gened
     DROP FOREIGN KEY gened_catalog;
 
-ALTER TABLE gened
+ALTER TABLE zeus_gened
     DROP FOREIGN KEY gened_course;
 
-ALTER TABLE major
+ALTER TABLE zeus_major
     DROP FOREIGN KEY major_catalog;
 
-ALTER TABLE major_course
+ALTER TABLE zeus_major_course
     DROP FOREIGN KEY major_course;
 
-ALTER TABLE major_course
+ALTER TABLE zeus_major_course
     DROP FOREIGN KEY major_courses;
 
-ALTER TABLE minor
+ALTER TABLE zeus_minor
     DROP FOREIGN KEY minor_catalog;
 
-ALTER TABLE minor_course
+ALTER TABLE zeus_minor_course
     DROP FOREIGN KEY minor_courses;
 
-ALTER TABLE minor_course
+ALTER TABLE zeus_minor_course
     DROP FOREIGN KEY minor_courses_course;
 
-ALTER TABLE plan
+ALTER TABLE zeus_plan
     DROP FOREIGN KEY plan_user;
 
-ALTER TABLE planned_concentration
+ALTER TABLE zeus_planned_concentration
     DROP FOREIGN KEY planned_concentration_concentration;
 
-ALTER TABLE planned_concentration
+ALTER TABLE zeus_planned_concentration
     DROP FOREIGN KEY planned_concentration_plan;
 
-ALTER TABLE planned_course
+ALTER TABLE zeus_planned_course
     DROP FOREIGN KEY planned_courses_course;
 
-ALTER TABLE planned_course
+ALTER TABLE zeus_planned_course
     DROP FOREIGN KEY planned_courses_plan;
 
-ALTER TABLE planned_major
+ALTER TABLE zeus_planned_major
     DROP FOREIGN KEY planned_major_major;
 
-ALTER TABLE planned_major
+ALTER TABLE zeus_planned_major
     DROP FOREIGN KEY planned_major_plan;
 
-ALTER TABLE planned_minor
+ALTER TABLE zeus_planned_minor
     DROP FOREIGN KEY planned_minor_minor;
 
-ALTER TABLE planned_minor
+ALTER TABLE zeus_planned_minor
     DROP FOREIGN KEY planned_minor_plan;
 
-ALTER TABLE prereq
+ALTER TABLE zeus_prereq
     DROP FOREIGN KEY prereqs_course1;
 
-ALTER TABLE prereq
+ALTER TABLE zeus_prereq
     DROP FOREIGN KEY prereqs_course2;
 
 -- tables
-DROP TABLE catalog;
+DROP TABLE zeus_catalog;
 
-DROP TABLE catalog_course;
+DROP TABLE zeus_catalog_course;
 
-DROP TABLE concentration;
+DROP TABLE zeus_concentration;
 
-DROP TABLE concentration_course;
+DROP TABLE zeus_concentration_course;
 
-DROP TABLE course;
+DROP TABLE zeus_course;
 
-DROP TABLE gened;
+DROP TABLE zeus_gened;
 
-DROP TABLE major;
+DROP TABLE zeus_major;
 
-DROP TABLE major_course;
+DROP TABLE zeus_major_course;
 
-DROP TABLE minor;
+DROP TABLE zeus_minor;
 
-DROP TABLE minor_course;
+DROP TABLE zeus_minor_course;
 
-DROP TABLE plan;
+DROP TABLE zeus_plan;
 
-DROP TABLE planned_concentration;
+DROP TABLE zeus_planned_concentration;
 
-DROP TABLE planned_course;
+DROP TABLE zeus_planned_course;
 
-DROP TABLE planned_major;
+DROP TABLE zeus_planned_major;
 
-DROP TABLE planned_minor;
+DROP TABLE zeus_planned_minor;
 
-DROP TABLE prereq;
+DROP TABLE zeus_prereq;
 
-DROP TABLE user;
+DROP TABLE zeus_user;
 
 -- End of file.
 
