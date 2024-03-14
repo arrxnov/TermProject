a920a1a4eae1cc1a890547b229f7c0fbf01b7af8
Jacob Grady <jcbgrd@gmail.com>
2024-03-14 13:34:45 -0400

    Add index.php

---
+++ project4/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    if (!isset($_SESSION["name"])) {
+       redirectToLogin();
+    }
+
+    $index = fopen("./index.html", "r");
+    while (!feof($index)) {
+        $line = fgets($index);
+        echo $line;
+     }
+
+     function redirectToLogin() {
+        echo "<script>";
+        echo "   window.location.replace('../login.php');";
+        echo "</script>";
+     }
+?>
