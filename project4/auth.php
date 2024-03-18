6609ba70cab6fe755e4ce61e387c278d8c5faba8
Jacob Grady <jcbgrd@gmail.com>
2024-03-18 11:04:43 -0400

    Add some files

---
+++ project4/auth.php
@@ -0,0 +1,10 @@
+<?php
+   echo "<html>";
+   echo hash("sha256", "password");
+   echo "<br>";
+   echo hash("sha256", "password");
+   echo "<br>"; 
+   echo hash("sha256", "password");
+   echo "<br>";
+   echo "</html>";
+?>
