9cd111423e0173fef6cb8615fd9265a3978183b2
Jacob Grady <jcbgrd@gmail.com>
2024-03-18 20:24:34 -0400

    Some updates

--- project4/auth.php
+++ project4/auth.php
@@ -2,10 +2,23 @@
    $user =  $_REQUEST["user"];
    $hash =  hash("sha256", $_REQUEST["pass"]);
 
-   $link = new mysqli("localhost", "tron", "*jksi72$", "test"); 
+   $link = new mysqli("localhost", "cs3220_sp24", "OqagokbAg9DzKZGb", "cs3220_sp24"); 
 
-   $sql = "SELECT password_hash WHERE stuid = ?";
+   $sql = "SELECT phash FROM zeus_user WHERE username = ?";
    $statement = $link->prepare($sql);
    $statement->execute([$user]);
-
+   $result = $statement->fetch();
+   echo $result;
+   if ($result != $hash) {
+      echo "<script>";
+      echo "   window.location.replace('./login.php');";
+      echo "</script>";
+   }
+   else {
+      session_start();
+      $_SESSION["name"] = $user;
+      echo "<script>";
+      echo "   window.location.replace('./index.php');";
+      echo "</script>";
+   }
 ?>
