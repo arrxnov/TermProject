b5ca4fc65d65f5c396583f237e73cfd7aaf030d0
Jacob Grady <jcbgrd@gmail.com>
2024-03-19 01:26:54 -0400

    Fixes

--- project4/get-json.php
+++ project4/get-json.php
@@ -13,7 +13,7 @@ function sqlQuery($link, $sql, $column, $constraint) {
 }
 
 if (!isset($_SESSION["name"])) {
-    header("Location: ./login.php");
+    header("Location: http://judah.cedarville.edu/~grady/TermProject/project4/login.php");
 }
 else {
     $dsn = "mysql:host=james.cedarville.edu;dbname=cs3220_sp24";
@@ -22,35 +22,28 @@ else {
     $link = new PDO($dsn, $sql_username, $password);
     $username = $_SESSION["name"];
     
-    $sql = "SELECT default_plan_id FROM zeus_user WHERE username = ?";
     $myObj = new stdClass();
+    $myObj->plan = new stdClass();
+
+    $sql = "SELECT default_plan_id FROM zeus_user WHERE username = ?";
     $plan_id = sqlQuery($link, $sql, "default_plan_id", $username);
     
-    $sql = "SELECT catalog_year FROM zeus_plan WHERE id = ?";
-    $myObj->catalog_year = new stdClass();
-    $myObj->catalog_year = sqlQuery($link, $sql, "catalog_year", $plan_id);
+    // $sql = "SELECT catalog_year FROM zeus_plan WHERE id = ?";
+    // $myObj->catalog = sqlQuery($link, $sql, "catalog_year", $plan_id);
 
     $sql = "SELECT major_id FROM zeus_planned_major WHERE plan_id = ?";
     $major_id = sqlQuery($link, $sql, "major_id", $plan_id);
     
     $sql = "SELECT name FROM zeus_plan WHERE id = ?";
-    $myObj->plan_name = sqlQuery($link, $sql, "name", $plan_id);
-
-    var_dump($plan_id);
-    var_dump($myObj->plan_name);
-    var_dump($myObj->catalog_year);
-    var_dump($major_id);
-
-    $sql = "SELECT name FROM zeus_plan WHERE id = ?";
-    $myObj->planName = sqlQuery($link, $sql, "name", $plan_id);
+    $myObj->plan->name = sqlQuery($link, $sql, "name", $plan_id);
 
     $sql = "SELECT name FROM zeus_major WHERE id = ?";
-    $major_name = sqlQuery($link, $sql, "name", $major_id);
+    $myObj->plan->major = sqlQuery($link, $sql, "name", $major_id);
     
-    $myObj->minor = "Bible";
+    $myObj->plan->minor = "Bible";
     
     $sql = "SELECT catalog_year FROM zeus_plan WHERE id = ?";
-    $myObj->year = sqlQuery($link, $sql, "year", $plan_id);
+    $myObj->year = sqlQuery($link, $sql, "catalog_year", $plan_id);
 
     $sql = "SELECT gpa FROM zeus_user WHERE username = ?";
     $myObj->gpa = sqlQuery($link, $sql, "gpa", $username);
@@ -59,56 +52,60 @@ else {
     $myObj->major_gpa = sqlQuery($link, $sql, "major_gpa", $username);
     
     $sql = "SELECT name FROM zeus_user WHERE username = ?";
-    $myObj->student_name = sqlQuery($link, $sql, "name", $username);
+    $myObj->plan->student = sqlQuery($link, $sql, "name", $username);
 
-    $currYear = date('Y');
+    $myObj->plan->currYear = date('Y');
     
     $month = date('m');
     if ($month < 8 && $month > 5) {
-        $currTerm = "SU";
+        $cmyObj->plan->urrTerm = "Summer";
     } else if ($month < 8) {
-        $currTerm = "SP";
+        $myObj->plan->currTerm = "Spring";
     } else {
-        $currTerm = "FA";
+        $myObj->plan->currTerm = "Fall";
     }
     
-    $myObj->current_semester = $currTerm.$currYear;
-    
-    $sql = "SELECT pc.term, pc.year, c.description as course_des, c.name as course_name, c.credits FROM zeus_planned_course as pc LEFT JOIN zeus_course as c ON pc.course_id = c.course_id";
-    $myObj->courses = sqlQuery($link, $sql, "stuff", $plan_id); // might need to be changed to get everything since sqlQuery gets index 0
-    // function sqlQuery($link, $sql, $column, $constraint) {
-    //     $statement = $link->prepare($sql);
-    //     $statement->execute([$constraint]);
-    //     $result = $statement->fetch();
-    //     return $result[$column];
-    // }
+    $sql = "SELECT course_id FROM zeus_planned_course WHERE plan_id = ?";
+    $statement = $link->prepare($sql);
+    $statement->execute([$plan_id]);
+    $result = $statement->fetchAll(PDO::FETCH_OBJ);
+    $ids = array_map(function($v){
+        return $v->course_id;
+    }, $result);
+    $myObj->courses = array();
+    foreach ($ids as $id) {
+        $myObj->courses[$id] = new stdClass();
+        
+        $sql = "SELECT term FROM zeus_planned_course WHERE course_id = ?";
+        $myObj->courses[$id]->term = sqlQuery($link, $sql, "term", $id);
+
+        $sql = "SELECT year FROM zeus_planned_course WHERE course_id = ?";
+        $myObj->courses[$id]->year = sqlQuery($link, $sql, "year", $id);
+
+        $myObj->courses[$id]->course_des = $id;
+
+        $sql = "SELECT name FROM zeus_course WHERE id = ?";
+        $myObj->courses[$id]->course_name = sqlQuery($link, $sql, "name", $id);
+
+        $sql = "SELECT credits FROM zeus_course WHERE id = ?";
+        $myObj->courses[$id]->credits = sqlQuery($link, $sql, "credits", $id);
+    }
+
+    // $sql = "SELECT pc.term, pc.year, c.description as course_des, c.name as course_name, c.credits FROM zeus_planned_course as pc LEFT JOIN zeus_course as c ON pc.course_id = c.course_id WHERE pc.plan_id = ?";
+    // $sql = "SELECT * FROM zeus_planned_course LEFT JOIN zeus_course ON zeus_planned_course.course_id = zeus_course.course_id WHERE zeus_planned_course.plan_id = ?";
+    // $statement = $link->prepare($sql);
+    // $statement->execute([$plan_id]);
+    // $result = $statement->fetch();
+    // var_dump($myObj->courses = $result[0]);
+    // // $myObj->courses = sqlQuery($link, $sql, "stuff", $plan_id); // might need to be changed to get everything since sqlQuery gets index 0
+    // // function sqlQuery($link, $sql, $column, $constraint) {
+    // //     $statement = $link->prepare($sql);
+    // //     $statement->execute([$constraint]);
+    // //     $result = $statement->fetch();
+    // //     return $result[$column];
+    // // }
     echo json_encode($myObj);
 
-
-//     $myObj->name = "John";
-//     $myObj->age = 30;
-//     $myObj->city = "New York";
-    
-    
-//     echo $myJSON;
-
-//     $obj = json_decode($_GET["x"], false);
-
-// $conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
-// $stmt = $conn->prepare("SELECT name FROM customers LIMIT ?");
-// $stmt->bind_param("s", $obj->limit);
-// $stmt->execute();
-// $result = $stmt->get_result();
-// $outp = $result->fetch_all(MYSQLI_ASSOC);
-
-// echo json_encode($outp);
-
-    
 }
-
-
-
-
-
 
 ?>
