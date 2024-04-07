5c6bc04f4de8a7d24f06fef2ad9785da911d4424
MtSneffels <113790394+MtSneffels@users.noreply.github.com>
2024-04-06 20:11:10 -0700

    removed unused code

--- project5/Olympus/Controllers/StudentDataController.cs
+++ project5/Olympus/Controllers/StudentDataController.cs
@@ -29,27 +29,9 @@ namespace Olympus.Controllers
                 .Select(c => new { c.id, c.name, c.credits, c.description });
 
             return Ok(JsonData);
-
-            //var courses = await (from c in new zeusContext().courses
-            //                     select new course { id = c.id, name = c.name, credits = c.credits, description = c.description }).ToListAsync();
-
-            //var courseId = await _context.courses.Where
-
-            //var JsonData = new { id = false, name = false, credits = false, description = false };
-
-            //var user = await _userManager.GetUserAsync(HttpContext.User);
-
-            //if (user == null)
-            //{
-            //    return NotFound();
-            //}
-
-            //return View(await _context.aspnetusers.Where(stu => (stu.advisors.Select(t => t.Id).ToList().Contains(user.Id)) || false).ToListAsync());
-
-            //return Ok(JsonData);
         }
 
-        [HttpGet("api/[controller]/[action]/{studentId}")]
+        [HttpGet("{studentId}")]
         public async Task<IActionResult> GetUserMetadata(string studentId)
         {
             var user = await _userManager.GetUserAsync(HttpContext.User);
@@ -59,42 +41,12 @@ namespace Olympus.Controllers
                 return Forbid();
             }
 
-            var JsonData = false;
+            var JsonData = new { };
 
             return Ok(JsonData);
         }
 
-        [HttpGet("api/[controller]/[action]/{studentId}")]
-        public async Task<IActionResult> GetPlannedCourses(string studentId)
-        {
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-
-            if ((HttpContext.User.IsInRole("Student")) && (user.Id != studentId))
-            {
-                return Forbid();
-            }
-
-            var JsonData = false;
-
-            return Ok(JsonData);
-        }
-
-        [HttpGet("api/[controller]/[action]/{studentId}")]
-        public async Task<IActionResult> GetRequirements(string studentId)
-        {
-            var user = await _userManager.GetUserAsync(HttpContext.User);
-
-            if ((HttpContext.User.IsInRole("Student")) && (user.Id != studentId))
-            {
-                return Forbid();
-            }
-
-            var JsonData = false;
-
-            return Ok(JsonData);
-        }
-
-        [HttpGet("api/[controller]/[action]/{studentId}")]
+        [HttpGet("{studentId}")]
         public async Task<IActionResult> GetPlans(string studentId)
         {
             var user = await _userManager.GetUserAsync(HttpContext.User);
@@ -104,7 +56,37 @@ namespace Olympus.Controllers
                 return Forbid();
             }
 
-            var JsonData = false;
+            var JsonData = new { };
+
+            return Ok(JsonData);
+        }
+
+        [HttpGet("{studentId}/{planId}")]
+        public async Task<IActionResult> GetPlannedCourses(string studentId, int planId)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if ((HttpContext.User.IsInRole("Student")) && (user.Id != studentId))
+            {
+                return Forbid();
+            }
+
+            var JsonData = new { };
+
+            return Ok(JsonData);
+        }
+
+        [HttpGet("{studentId}/{planId}")]
+        public async Task<IActionResult> GetRequirements(string studentId, int planId)
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+
+            if ((HttpContext.User.IsInRole("Student")) && (user.Id != studentId))
+            {
+                return Forbid();
+            }
+
+            var JsonData = new { };
 
             return Ok(JsonData);
         }
