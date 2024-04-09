329d6ddca14a5a0e3e93429949f860a07f2d778c
MtSneffels <113790394+MtSneffels@users.noreply.github.com>
2024-04-09 12:57:43 -0700

    Fixed javascript

--- project5/Olympus/wwwroot/js/student.js
+++ project5/Olympus/wwwroot/js/student.js
@@ -54,12 +54,12 @@ async function initPage() {
     var plannedCourses = data2[1];
     var requirements = data2[2];
 
-    console.log(allCourseData);
-    console.log(userMetadata);
-    console.log(plans);
-    console.log(planMetadata);
-    console.log(plannedCourses);
-    console.log(requirements);
+    //console.log(allCourseData);
+    //console.log(userMetadata);
+    //console.log(plans);
+    //console.log(planMetadata);
+    //console.log(plannedCourses);
+    //console.log(requirements);
 
     populateSearchTable(allCourseData);
     populatePlanDropdown(plans)
@@ -68,212 +68,8 @@ async function initPage() {
     populateCourses(plannedCourses, allCourseData);
     populateRequirements(requirements, allCourseData);
 
-    //jQuery(function () {
-    //    jQuery('ul.menu li').hover(function () {
-    //        jQuery(this).children('ul').delay(10).slideDown(100);
-    //    }, function(){
-    //        jQuery(this).children('ul').delay(10).slideUp(100);
-    //    });
-    //});
-
-    //let dropdownPlans = await getPlans();
-    //let planDropdown = jQuery("#planSubMenu");
-    //jQuery.each(dropdownPlans, function(name, id) {
-    //    planDropdown.append(
-    //        jQuery("<li></li>").html("<p>" + name + "</p>").attr("id", id)
-    //    );
-    //    jQuery("#" + id).click(function () {
-    //        window.location.search = '?planId=' + id;
-    //    });
-    //});
-
-    //let response = await getCombined(plan_id);
-
-    //for (let course in response.catalog.courses) {
-    //    courseNames[course] = {
-    //        "name": response.catalog.courses[course].name,
-    //        "credits": response.catalog.courses[course].credits
-    //    };
-    //}
-
-    //let plan = response.plan;
-    //planToYear(plan);
-    //updateCourses(plan);
-    //await updateReqs();
-    //jQuery("#courseReqs").accordion({ collapsible: true, });
+    console.log("Page loading complete")
 }
-
-//async function getPlans() {
-//    const response = await fetch("./get-plans.php");
-//    const data = await response.json();
-//    return data;
-//}
-
-//async function getCombined() {
-//    if (!getUrlParameter("planId")) {
-//        fetch_string = "./get-json.php";
-//    } else {
-//        fetch_string = "./get-json.php?" + new URLSearchParams({
-//            planId: getUrlParameter("planId"),
-//        });
-//    }
-//    const response = await fetch(fetch_string);
-//    const data = await response.json();
-//    return data;
-//}
-
-//async function getRequirements() {
-//    if (!getUrlParameter("planId")) {
-//        fetch_string = "./get-requirements.php";
-//    } else {
-//        fetch_string = "./get-requirements.php?" + new URLSearchParams({
-//            planId: getUrlParameter("planId"),
-//        });
-//    }
-//    const response = await fetch(fetch_string);
-//    const data = await response.json();
-//    return data;
-//}
-
-//function getCourseName(courseId) {
-//    return courseNames[courseId].name;
-//}
-
-//function getCourseCredits(courseId) {
-//    return courseNames[courseId].credits;
-//}
-
-//function planToYear(planJSON) {
-//    for (let course in planJSON["courses"]) {
-//        course = planJSON["courses"][course];
-//        let y = course["year"];
-//        let t = course["term"];
-//        if (!(y in years)) {
-//            years[y] = {
-//                Fall: [],
-//                Spring: [],
-//                Summer: []
-//            };
-//        }
-//        years[y][t].push(course);
-//    }
-//    return years;
-//}
-
-//async function updateReqs() {
-//    reqs = await getRequirements();
-//    for (let course in reqs.categories.Cognates.courses) {
-//        course = reqs.categories.Cognates.courses[course];
-//        let courseName = getCourseName(course);
-//        document.getElementById("cognates").innerHTML += `<p id=${global_noncollision++} class=\"course\" draggable=\"true\" ondragstart=\"dragStartHandler2(event)\"> <span class=\"course-id\">` + course + "</span> " + courseName + "</p>";
-//    }
-//    for (let course in reqs.categories.Electives.courses) {
-//        course = reqs.categories.Electives.courses[course];
-//        let courseName = getCourseName(course);
-//        document.getElementById("electives").innerHTML += `<p id=${global_noncollision++} class=\"course\" draggable=\"true\" ondragstart=\"dragStartHandler2(event)\"> <span class=\"course-id\">` + course + "</span> " + courseName + "</p>";
-//    }
-//    for (let course in reqs.categories.Core.courses) {
-//        course = reqs.categories.Core.courses[course];
-//        let courseName = getCourseName(course);
-//        document.getElementById("core").innerHTML += `<p id=${global_noncollision++} class=\"course\" draggable=\"true\" ondragstart=\"dragStartHandler2(event)\"> <span class=\"course-id\">` + course + "</span> " + courseName + "</p>";
-//    }
-//    for (let course in reqs.categories.GenEds.courses) {
-//        course = reqs.categories.GenEds.courses[course];
-//        let courseName = getCourseName(course);
-//        document.getElementById("geneds").innerHTML += `<p id=${global_noncollision++} class=\"course\" draggable=\"true\" ondragstart=\"dragStartHandler2(event)\"> <span class=\"course-id\">` + course + "</span> " + courseName + "</p>";
-//    }
-//}
-
-//function updateCourses(planJSON) {
-//    let header = document.getElementById("planHeader");
-//    header.innerHTML = "";
-//    header.innerHTML += "<p><strong>Student:</strong> " + planJSON["student"] + "</p>\n";
-//    header.innerHTML += "<p><strong>Academic Plan:</strong> " + planJSON["name"] + "</p>\n";
-//    let header2 = document.getElementById("planSubheader");
-//    header2.innerHTML += "<p><strong>Major:</strong> " + planJSON["major"] + "</p>\n";
-//    header2.innerHTML += "<p><strong>Minor:</strong> " + planJSON["minor"] + "</p>\n";
-//    header2.innerHTML += "<p><strong>Catalog:</strong> " + planJSON["catYear"] + "</p>\n";
-//    header2.innerHTML += "<p><strong>GPA:</strong> " + planJSON["gpa"] + "</p>\n";
-//    header2.innerHTML += "<p><strong>Major GPA:</strong> " + planJSON["major_gpa"] + "</p>\n";
-
-//    let totalCreds = 0;
-//    let pastSemester = true;
-
-//    for (let i = 1; i <= 12; i++) {
-//        let semester = document.getElementById("semester" + i);
-//        let base_y = planJSON["catYear"];
-//        let y = parseInt(base_y) + parseInt((i + 1) / 3);
-//        let t;
-//        let term;
-
-//        switch (i % 3) {
-//            case 0:
-//                t = "Summer";
-//                term = "Summer";
-//                break;
-//            case 1:
-//                t = "Fall";
-//                term = "Fall";
-//                break;
-//            case 2:
-//                t = "Spring";
-//                term = "Spring";
-//                break;
-//        }
-
-//        term += " " + y;
-//        semester.getElementsByClassName("term")[0].innerHTML = term;
-
-//        if (t == planJSON["currTerm"] && y == planJSON["currYear"]) {
-//            semester.getElementsByClassName("term")[0].innerHTML += " (Current)";
-//            currentSemester = true;
-//            semester.classList.add("semester-current");
-//            pastSemester = false;
-
-//        }
-//        else currentSemester = false;
-
-//        if (pastSemester) {
-//            semester.classList.remove("semester");
-//            semester.classList.add("semester-past");
-//        }
-
-//        if (!(y in years)) {
-//            continue;
-//        }
-
-//        let courses = years[y][t];
-//        let credits = 0;
-
-//        for (let key in courses) {
-//            let course = courses[key];
-//            credits += parseFloat(getCourseCredits(course["id"]));
-//            let c_str = parseFloat(getCourseCredits(course["id"])).toPrecision(2);
-//            if (!pastSemester && !currentSemester) {
-//                semester.innerHTML += `<p id=${global_noncollision++} class=\"course\" draggable=\"true\" ondragstart=\"dragStartHandler(event)\"> <span class=\"course-id\">` + course["id"] + "</span> " + getCourseName(course["id"]) + "<span class=\"course-credits\">" + c_str + "</span>" + "</p>\n";
-//            } else {
-//                semester.innerHTML += `<p id=${global_noncollision++} class=\"course\"> <span class=\"course-id\">` + course["id"] + "</span> " + getCourseName(course["id"]) + "<span class=\"course-credits\">" + c_str + "</span>" + "</p>\n";
-//            }
-//        }
-
-//        totalCreds += parseFloat(credits);
-//        let year = semester.getElementsByClassName("credits")[0];
-//        if (credits >= 10) {
-//            year.innerHTML = "Credits: " + parseFloat(credits).toPrecision(3) + "";
-//        } else {
-//            year.innerHTML = "Credits: " + parseFloat(credits).toPrecision(2) + "";
-//        }
-
-
-//    }
-//    let prec = 2;
-//    if (totalCreds >= 10) {
-//        prec = 3;
-//    } else if (totalCreds >= 100) {
-//        prec = 4;
-//    }
-//    header.innerHTML += "<p><strong>Total Hours:</strong> " + totalCreds.toPrecision(prec) + "</p>\n";
-//}
 
 function setupHandlers() {
     jQuery("#jgradyBtn").click(function () {
@@ -358,12 +154,22 @@ function populateYears(plannedcourses) {
         document.getElementById("plan").innerHTML += "<div id=\"year" + i + "\" class=\"year\"></div>";
     }
     let currentYear = minYear;
-    let calyear = 2024;
-    let calTerm = "SP";
     for (let year of document.getElementsByClassName("year")) {
-        year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester\"><div class=\"semesterHeader\"><div class=\"term\">Fall " + (currentYear) + "<span class=\"semesterCredits\"></span></div>";
-        year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester\"><div class=\"semesterHeader\"><div class=\"term\">Spring " + (currentYear+1) + "<span class=\"semesterCredits\"></span></div>";
-        year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester\"><div class=\"semesterHeader\"><div class=\"term\">Summer " + (currentYear+1) + "<span class=\"semesterCredits\"></span></div>";
+        if (currentYear < 2024) {
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester-past\"><div class=\"semesterHeader\"><div class=\"term\">Fall " + (currentYear) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester-past\"><div class=\"semesterHeader\"><div class=\"term\">Spring " + (currentYear + 1) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester-past\"><div class=\"semesterHeader\"><div class=\"term\">Summer " + (currentYear + 1) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+        } else if (currentYear == 2024) {
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester-past\" ondrop=\"dropHandler(event, this)\" ondragover=\"dragOverHandler(event,this)\"><div class=\"semesterHeader\"><div class=\"term\">Fall " + (currentYear) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester semester-current\" ondrop=\"dropHandler(event, this)\" ondragover=\"dragOverHandler(event,this)\"><div class=\"semesterHeader\"><div class=\"term\">Spring " + (currentYear + 1) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester\" ondrop=\"dropHandler(event, this)\" ondragover=\"dragOverHandler(event,this)\"><div class=\"semesterHeader\"><div class=\"term\">Summer " + (currentYear + 1) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+
+        } else {
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester\" ondrop=\"dropHandler(event, this)\" ondragover=\"dragOverHandler(event,this)\"><div class=\"semesterHeader\"><div class=\"term\">Fall " + (currentYear) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+            year.innerHTML += "<div id=\"" + global_noncollision++ + "\" class=\"semester\" ondrop=\"dropHandler(event, this)\" ondragover=\"dragOverHandler(event,this)\"><div class=\"semesterHeader\"><div class=\"term\">Spring " + (currentYear + 1) + "</div><span class=\"semesterCredits\">Credits: </span></div>";
+        }
+
+        currentYear++;
     }
 }
 
@@ -439,13 +245,12 @@ function populateCourses(plannedcourses, allcourses) {
         let found = false;
         for (let course of allcourses) {
             if (course_id == course["id"]) {
-                course_desc = course["description"];
+                course_desc = course["name"];
                 c_str = course["credits"];
                 break;
             }
         }
         let year = plancourse["year"];
-        console.log(year);
         let term = plancourse["term"];
         if (term == "FA") term = "Fall";
         else if (term == "SP") term = "Spring";
@@ -453,19 +258,17 @@ function populateCourses(plannedcourses, allcourses) {
         else console.log("How did we get here");
         let future_semesters = document.getElementsByClassName("semester");
         for (let semester of future_semesters) {
-            console.log(semester);
-            if (term in semester.getElementsByClassName("term") && (year) in semester.getElementsByClassName("term")) {
+            if (semester.getElementsByClassName("term")[0].innerHTML.indexOf(term) !== -1 && (semester.getElementsByClassName("term"))[0].innerHTML.indexOf(year) !== -1) {
                 semester.innerHTML += `<p id=${global_noncollision++} class=\"course\" draggable=\"true\" ondragstart=\"dragStartHandler(event)\"> <span class=\"course-id\">` + course_id + "</span> " + course_desc + "<span class=\"course-credits\">" + c_str + "</span>" + "</p>\n";
                 found = true;
                 break;
             }
-            else console.log("Evaling false");
         }
         if (found) continue;
         for (let semester of document.getElementsByClassName("semester-past")) {
             console.log(semester);
-            if (term in semester.getElementsByClassName("term") && (year) in semester.getElementsByClassName("term")) {
-                semester.innerHTML += `<p id=${global_noncollision++} class=\"course\"> <span class=\"course-id\">` + course_id + "<span> " + course_desc + "<span class=\"course-credits\">" + c_str + "</span>" + "</p>\n";
+            if (semester.getElementsByClassName("term")[0].innerHTML.indexOf(term) !== -1 && (semester.getElementsByClassName("term")[0]).innerHTML.indexOf(year) !== -1) {
+                semester.innerHTML += `<p id=${global_noncollision++} class=\"course\"> <span class=\"course-id\">` + course_id + "</span> " + course_desc + "<span class=\"course-credits\">" + c_str + "</span>" + "</p>\n";
                 found = true;
                 break;
             }
@@ -473,10 +276,10 @@ function populateCourses(plannedcourses, allcourses) {
         }
         if (found) continue;
         let semester = document.getElementsByClassName("semester-current")[0];
-        console.log(semester);
-        console.log((year));
-        console.log(term);
-        if (semester && term in semester.getElementsByClassName("term")[0] && (year) in semester.getElementsByClassName("term")[0]) {
+        //console.log(semester);
+        //console.log((year));
+        //console.log(term);
+        if (semester.getElementsByClassName("term")[0].innerHTML.indexOf(term) !== -1 && (semester.getElementsByClassName("term")[0]).innerHTML.indexOf(year) !== -1) {
             semester.innerHTML += `<p id=${global_noncollision++} class=\"course\"> <span class=\"course-id\\` + course_id + "</span> " + course_desc + "<span class=\"course-credits\">" + c_str + "</span>" + "</p>\n";
         }
         else console.log("Evaling false");
@@ -484,8 +287,6 @@ function populateCourses(plannedcourses, allcourses) {
 }
 
 function populateHeader(userdata, plandata) {
-    console.log(userdata);
-    console.log(plandata);
     let header = document.getElementById("planHeader");
     header.innerHTML += "<p><strong>Student:</strong> " + userdata["name"] + "</p>";
     header.innerHTML += "<p><strong>Plan:</strong> " + plandata["name"] + "</p>";
@@ -520,12 +321,7 @@ function dropHandler(ev, el) {
     } else {
         el.appendChild(document.getElementById(data));
     }
-    checkRequirments();
-}
-
-function dropHandler2(ev, el) {
-    ev.preventDefault();
-    document.getElementById(ev.dataTransfer.getData("Text")).parentElement.remove(document.getElementById(ev.dataTransfer.getData("Text")));
+    checkRequirements();
 }
 
 function dragOverHandler(ev) {
@@ -533,7 +329,7 @@ function dragOverHandler(ev) {
 }
 
 for (let course in document.getElementsByClassName("course")) {
-    course.onmouseup = function(event) {
+    course.onmouseup = function (event) {
         if (event.which == 3) {
             removeElement(event.target);
             checkRequirements();
