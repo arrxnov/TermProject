9942d74c365c6916ac144a15b15e5ebfc22697b9
Logan Miller <lj@mountainmillers.com>
2024-04-22 19:04:11 -0400

    Added student, faculty, and admin views to server directory

--- project6/server/app.js
+++ project6/server/app.js
@@ -2,8 +2,9 @@ import React from 'react';
 import 'path';
 
 const express = require('express');
-
+const db = require('./db/database');
 const app = express();
+const router = express.Router();
 
 app.use(cors()); // lock this down
 
