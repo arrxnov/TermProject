9a40771f27bb147dfaac628894d41ed3e078d059
Logan Miller <lj@mountainmillers.com>
2024-04-23 15:24:36 -0400

    edited studentdata

--- project6/backend/routes/studentdata.js
+++ project6/backend/routes/studentdata.js
@@ -2,9 +2,13 @@ var express = require('express');
 var zeus = require('../db/database');
 var router = express.Router();
 
-/* GET users listing. */
+// call Kai's api and pass session id, user id, plan -d (default user and plan ids)
+// have him return valid or not
+
+// if not Kai's api
+//     abort
+
 router.get('/', function(req, res, next) {
   res.send({"name": "Logan Miller", "gpa": 3.00, "major_gpa": 3.05, "default_plan_id" : 1});
 });
-
-module.exports = router;
+module.exports = router;s
