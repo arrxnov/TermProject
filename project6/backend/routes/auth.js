4a9e9eda468193ebdadd7111e5e955b029e646f5
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-04-28 18:30:56 -0400

    Finished /role path and fixed several bugs

--- project6/backend/routes/auth.js
+++ project6/backend/routes/auth.js
@@ -3,12 +3,11 @@ var zeus = require('../db/database');
 var router = express.Router();
 
 router.get('/login/:uname/:phash', function(req, res, next) {
-    // add url parameters above
     /*
     aspnetusers
-- Id (hash)
-- UserName (string)
-- PasswordHash (hash)
+    - Id (hash)
+    - UserName (string)
+    - PasswordHash (hash)
     */
     sql = "SELECT Id FROM aspnetusers WHERE UserName = ? and PasswordHash = ?";
     queryResult = null;
@@ -21,17 +20,16 @@ router.get('/login/:uname/:phash', function(req, res, next) {
     });
 
     if (Object.keys(queryResult).length == 0) {
-        res.send({"valid": false});
+        res.status(400);
+        res.send("Invalid login credentials\n");
+    } else {
+        res.send({"valid": true, "id": queryResult['id']});
     }
-
-    // query db if necessary and res.send(json) or ok messsage
-    res.send({"valid": true, "id": queryResult['id']});
-    // if error, send appropriate error code and message
 });
 
 router.get('/logout/:sessionId', function(req, res, next) {
     // add session parameter above
-    sql = "DELETE * FROM aspnetusersFROM aspnetusertokens WHERE Value = ?";
+    sql = "DELETE FROM aspnetusertokens WHERE Value = ? RETURNING UserId";
     queryResult = null;
     zeus.query(sql, [req.params.sessionId], (error, results) => {
         if (error) {
@@ -40,48 +38,58 @@ router.get('/logout/:sessionId', function(req, res, next) {
         }
         queryResult = results.map(v => Object.assign({}, v));
     });
-});
-
-router.get('/role/:', function(req, res, next) {
-    // query db if necessary and res.send(json) or ok messsage
-    res.send({"something": "else", "about": "me"});
-
-    // if error, send appropriate error code and message
-});
-
-
-function validateFaculty(sessionId) {
-    let queryResult = getUserIdFromSession(sessionId);
-    let userId = null; 
-    
     if (Object.keys(queryResult).length == 0) {
-        return {"valid": false};
+        res.status(400);
+        res.send("Logout failed\n");
     } else {
-        if (!queryResult["valid"]) {
-            return queryResult;
-        }
-        userId = queryResult["UserId"];
+        res.send(queryResult["UserId"]);
     }
+});
 
-    // check user has faculty role
-    sql = "SELECT * FROM aspnetuserroles WHERE user_id = ? and role = 2";
-    queryResult = null;
-    zeus.query(sql, [userId], (error, results) => {
-        if (error) {
-            console.log(sql + " failed");
-            return console.error(error.message);
-        }
-        queryResult = results.map(v => Object.assign({}, v));
-    });
+router.get('/role/:sessionId', function(req, res, next) {
+    let queryResult = getUserIdFromSession(req.params.sessionId);
+    let userId = null; 
+    let role = null;
 
-    if (Object.keys(queryResult).length == 0) {
-        return {"valid": false};
-    } 
 
-    const validSession = {"valid": true, "facultyId": userId};
-    
-    return validSession;
-}
+    if (Object.keys(queryResult).length == 0 || !queryResult["valid"]) {
+        res.send(queryResult);
+    } else {
+        userId = queryResult["UserId"];
+
+        sql = "SELECT role FROM aspnetuserroles WHERE user_id = ?";
+        queryResult = null;
+        zeus.query(sql, [userId], (error, results) => {
+            if (error) {
+                console.log(sql + " failed");
+                return console.error(error.message);
+            }
+            queryResult = results.map(v => Object.assign({}, v));
+        });
+
+        if (Object.keys(queryResult).length == 0) {
+            res.status(400);
+            res.send('User \'s roles are not properly configured\n');
+            return;
+        } 
+        switch(queryResult["role"]) {
+            case 1:
+                role = "Admin";
+            case 2:
+                role = "Faculty";
+            case 3:
+                role = "Student";
+            }
+        if (role == null) {
+            res.send('Invalid role number\n');
+        } else {
+            const validSession = {"valid": true, "role": role, "userId": userId};
+            res.send(validSession);
+        }        
+    }
+});
+
+
 
 
 function getUserIdFromSession(sessionId) {
@@ -109,18 +117,45 @@ function getUserIdFromSession(sessionId) {
     return {"valid": true, "userId": userId};
 }
 
-function validateStudent(sessionId, studentId=null) {
-    let queryResult = getUserIdFromSession(sessionId);
-    let role = "";
+
+function validateFaculty(sessionId) {
     let userId = null; 
+    
+    let queryResult = getUserIdFromSession(sessionId);
+    if (Object.keys(queryResult).length != 0 && queryResult["valid"]) {
+        userId = queryResult["UserId"];
+    } else {
+        res.send(queryResult);
+    }
+
+    // check user has faculty role
+    sql = "SELECT * FROM aspnetuserroles WHERE user_id = ? and role = 2";
+    queryResult = null;
+    zeus.query(sql, [userId], (error, results) => {
+        if (error) {
+            console.log(sql + " failed");
+            return console.error(error.message);
+        }
+        queryResult = results.map(v => Object.assign({}, v));
+    });
 
     if (Object.keys(queryResult).length == 0) {
         return {"valid": false};
     } else {
-        if (!queryResult["valid"]) {
-            return queryResult;
-        }
+        return {"valid": true, "facultyId": userId};
+    }
+}
+
+
+function validateStudent(sessionId, studentId=null) {
+    let role = "";
+    let userId = null; 
+    
+    let queryResult = getUserIdFromSession(sessionId);
+    if (Object.keys(queryResult).length != 0 && queryResult["valid"]) {
         userId = queryResult["UserId"];
+    } else {
+        res.send(queryResult);
     }
 
     
@@ -147,19 +182,17 @@ function validateStudent(sessionId, studentId=null) {
         role = "student";
         studentId = userId;
     }
-
-    const validSession = {"valid": true, "role": role, "studentId": studentId};
-    
-    return validSession;
+        
+    return {"valid": true, "role": role, "studentId": studentId};
 }
 
 function validatePlan(sessionId, planId, studentId=null) {
     queryResult = validateStudent(sessionId, studentId);
-    if (queryResult["valid"]) {
+    if (!queryResult["valid"]) {
+        return queryResult;
+    } else {
         role = queryResult["role"];
         studentId = queryResult["studentId"];
-    } else {
-        return queryResult;
     }
 
     // check that plan belongs to studentId
