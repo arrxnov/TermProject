1b5117bb33380ec28e653961dac61e528ee0eda5
Logan Miller <lj@mountainmillers.com>
2024-04-30 23:23:27 -0400

    aborted auth

--- project6/src/Login.jsx
+++ project6/src/Login.jsx
@@ -3,53 +3,53 @@ import PropTypes from 'prop-types';
 
 async function loginUser(credentials) {
     console.log(JSON.stringify(credentials)); // FIXME
-  
+
     return fetch('http://localhost:3000/auth/login', {
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json'
-     },
-     body: JSON.stringify(credentials)
-   })
-     .then(data => data.json())
-  }
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
   
-  export default function Login({ setToken, setRole }) {
+export default function Login({ setToken, setRole }) {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
-  
+
     const handleSubmit = async e => {
-      e.preventDefault();
-      const response = await loginUser({
+        e.preventDefault();
+        const response = await loginUser({
         username,
         password
-      });
-      console.log(response); // FIXME
-      setToken(response.sessionId);
-      setRole(response.role);
+        });
+        console.log(response); // FIXME
+        setToken(response.sessionId);
+        setRole(response.role);
     }
-  
+
     return(
-      <div className="login-wrapper">
+        <div className="login-wrapper">
         <h1>Please Log In</h1>
         <form onSubmit={handleSubmit}>
-          <label>
+            <label>
             <p>Username</p>
             <input type="text" onChange={e => setUserName(e.target.value)} />
-          </label>
-          <label>
+            </label>
+            <label>
             <p>Password</p>
             <input type="password" onChange={e => setPassword(e.target.value)} />
-          </label>
-          <div>
+            </label>
+            <div>
             <button type="submit">Submit</button>
-          </div>
+            </div>
         </form>
-      </div>
+        </div>
     )
-  }
+}
   
-  Login.propTypes = {
+Login.propTypes = {
     setToken: PropTypes.func.isRequired,
     setRole: PropTypes.func.isRequired
-  };
+};
